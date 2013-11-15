f9b94dc801623288f1ed82adb2d5a606dd2b00f8
Will <will.a.ft@gmail.com>
2013-11-14 21:19:52 -0500

    CLI for testing (to be removed)

--- movie_fetcher.py
+++ movie_fetcher.py
@@ -12,9 +12,7 @@ itunes_terms = {
 }
 
 def extract_terms(jsonr, terms):
-    ret = {}
-    for key in terms:
-        ret[key] = jsonr[terms[key]]
+    ret = {k:jsonr[terms[k]] for k in terms}
     return ret
 
 def itunes_json(term):
@@ -22,7 +20,16 @@ def itunes_json(term):
     url += urlencode({'term':term, 'media':'movie'})
     return json.load(urllib2.urlopen(url))
 
+def printd(d):
+    print '%s'%d['title']
+    print '[ %.2f ]'%d['price']
+    print '\n\t%s'%d['desc']
+
 #print [k for k in itunes_json(raw_input('Lookup in iTunes: '))['results'][0]]
 if __name__ == '__main__':
-    itj = itunes_json(raw_input('Lookup in iTunes: '))['results'][0]
-    print extract_terms(itj, itunes_terms)
+    print '-'*80
+    try:
+        itj = itunes_json(raw_input('Lookup in iTunes: '))['results'][0]
+        printd(extract_terms(itj, itunes_terms))
+    except:
+        print 'Lookup failed.'

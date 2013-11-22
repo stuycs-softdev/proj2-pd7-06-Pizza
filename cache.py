b51fc0c6b0517988b4642adebc97549055d9c428
Will <will.a.ft@gmail.com>
2013-11-21 21:02:46 -0500

    caching complete

--- cache.py
+++ cache.py
@@ -4,18 +4,19 @@ from datetime import datetime
 _cache = MongoClient().db.cache
 
 def check_cache(title):
-    res = _cache.find_one({'title':title})
+    res = _cache.find_one({'title_lower':title.lower()})
     if (res is None or _isexpired(res['mod_date'])):
         return False
     return True
 
 def retrieve_cached(title):
-    return _cache.find_one({'title':title})
+    return _cache.find_one({'title_lower':title.lower()})
 
 def cache(d):
+    d['title_lower'] = d['title'].lower()
     d['mod_date'] = datetime.now()
     _cache.update({'title':d['title']},
-                  d, {'upsert':True}
+                  d, upsert=True
     )
 
 def _isexpired(date):
